ee886880bd77b9ea8f6eacc712da86e5b12f774a
Adam Zimmerman <azimmerman2@usfca.edu>
2020-09-22 09:22:52 -0700

    Fixed variable name based on lintr

--- code/02_count_counties_cities.R
+++ code/02_count_counties_cities.R
@@ -14,9 +14,11 @@ input_file_name <- "output/covid_mobility_data_Nevada.csv"
 state_data <- read.csv(input_file_name)
 
 # starting off with dplyr chains
-count_of_cities_counties_by_transit <- state_data %>% 
+count_cities_counties_transit <- state_data %>%
   select(geo_type, region, transportation_type) %>%
   group_by(geo_type, transportation_type) %>%
   tally()
-write.csv(count_of_cities_counties_by_transit,
+
+# write out the result of dyplr chain
+write.csv(count_cities_counties_transit,
           "output/nevada_cities_counties_counts.csv")
