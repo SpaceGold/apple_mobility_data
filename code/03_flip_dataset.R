f27ae13a609b48f72186dc20d6064e7f74a7fbec
Adam Zimmerman <azimmerman2@usfca.edu>
2020-09-26 11:51:00 -0700

    Finish working build of pivot_longer exercise.

---
+++ code/03_flip_dataset.R
@@ -0,0 +1,18 @@
+# Call a function that reads in a dataset, flips it to be long instead
+# of wide, and then checks to make sure it's correct.
+
+# Adam Zimmerman
+# 2020-09-25
+# adam.eric.zimmerman@gmail.com
+
+# file.path() is best practice for filenames
+
+# Insert filename or rewrite this to run on all *applemobilitytrends.csv
+input_filename <- "data/raw_data/applemobilitytrends-2020-09-19.csv"
+
+# Run the function 
+flip_dataset(input_filename)
+
+# Report
+print("Done.")
+
--- code/functions/flip_dataset.R
+++ code/functions/flip_dataset.R
@@ -13,41 +13,32 @@ library("tools")
 # TESTING:
 input_filename <- "data/raw_data/applemobilitytrends-2020-09-19.csv"
 
-
 # create a function to flip dates from columns to rows
 flip_dataset <- function(input_filename) {
 
   # read data
   our_dataset <- read_csv(input_filename)
 
-# get number of columns
-ncol_initial <- ncol(our_dataset)
+  # get number of columns
+  ncol_initial <- ncol(our_dataset)
 
-# before flipping, try converting colnames to char
-col_names_all <- colnames(our_dataset)
-col_names_char <- toString(col_names_all)
+  # flip. Currently this is clumsy, relying on literal names
+  final_dataset <- our_dataset %>%
+    pivot_longer("2020-01-13":"2020-09-19", names_to = "dates",
+                 values_to = "traffic_percentage")
 
-#will this work?
-colnames(our_dataset) <- col_names_char
-
-# flip
-our_dataset %>%
-  pivot_longer(!geo_type, !region, !transportation_type, !alternative_name,
-               !sub.region, !country, names_to = "dates",
-               values_to = "traffic_percentage")
-
-# get number of columns
-ncol_final <- ncol(our_dataset)
-
-# Defensive check: is dataframe longer and narrower?
-if (ncol_final >= ncol_initial) {
-  stop("ERROR: Flipping date and percentage data failed. Check raw data.")
-}
-
-# save
-fn <- sub(".csv", "", input_filename)
-fp <- paste(fn, "_long.csv", sep = "")
-write.csv(fp)
+  # get number of columns
+  ncol_final <- ncol(final_dataset)
+  
+  # Defensive check: is dataframe longer and narrower?
+  if (ncol_final >= ncol_initial) {
+    stop("ERROR: Flipping date and percentage data failed. Check raw data.")
+  }
+  
+  # save
+  fn <- sub(".csv", "", input_filename)
+  fp <- paste(fn, "_long.csv", sep = "")
+  write_csv(final_dataset, fp)
 }
 
 # Call function for testing
