47c35a4ef86a0dc5c0d16d59087531d0bff8de0f
Adam Zimmerman <azimmerman2@usfca.edu>
2020-10-12 10:43:48 -0700

    Add example state and Oct 10 data to params

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -3,4 +3,6 @@
 # A bash script to drive the rendering of an Rmarkdown file using the
 # command line; passing parameters from the shell
 
-Rscript -e "rmarkdown::render('Analysis.rmd')"
+Rscript -e "rmarkdown::render('Analysis.rmd',
+            params = list(state = 'West Virginia',
+            data = 'data/raw_data/applemobilitytrends-2020-10-10.csv))"
