15638ca194ca56f37b64bdaa23b614d555c2f605
Adam Zimmerman <azimmerman2@usfca.edu>
2020-11-10 17:20:53 +0000

    Update Rmd bash script to take seq summary data

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # A bash script to drive the rendering of an Rmarkdown file using the
 # command line; passing parameters from the shell
 
-# This script expects two parameters.
-# The first should be a US state, and the second is raw Apple Mobility Data fp
+# This script expects multiple parameters.
+# The first should be a US state, and the second is raw Apple Mobility Data
+# filepath, and the third is
 
 # Adam Zimmerman
 # adam.eric.zimmerman@gmail.com
@@ -12,15 +15,14 @@
 
 if [ $# -eq 0 ]
 then
-  echo "To run this script, supply state and raw data .csv params."
+  echo "Supply params: US state, raw data CSV, and seq summary."
   exit 1
 fi 
 
 state=$1
 state=${state// /_}
 
-Rscript -e "rmarkdown::render('Analysis.Rmd',\
-            params = list(state = '$1',\
-            data = '$2'),\
-            output_dir = 'output',\
-            output_file = 'Analysis_$state')"
+RMD_PARAMS="params = list(state = '$1', data = '$2', seqdata = '%3')"
+RMD_OUTPUT="output_dir = 'output', output_file = 'Analysis_$state'"
+
+Rscript -e "rmarkdown::render('Analysis.Rmd', $RMD_PARAMS, $RMD_OUTPUT)"
