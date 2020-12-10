0a414207e1318c66c66a85da282609c68bd963c9
Adam Zimmerman <azimmerman2@usfca.edu>
2020-12-10 04:58:54 +0000

    Backup fixed script re:ALL that breaks downstream analysis

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 
 # A bash script to analyze sequencing data from COVID-19.
 
-# This script expects one parameter, a fasta file, with | chars,
-# and optionally "ALL" as a second parameter.
+# This script expects one parameter, a fasta file, with | chars.
 
 # This script outputs the total number of sequences in the file.
 # It also tallies the number of sequences in the dataset from each
@@ -23,33 +22,38 @@ then
 fi
 
 # Assign input file, e.g. 150 MB compressed .fasta
-fasta_gz_input=$1 | \
-
-# Pipe just opening lines with grep. Don't store whole lines!
-zgrep "^>" "$fasta_gz_input" | \
-
-# Replace space with \s; Replace | with \t
-sed -E 's/\|/\t/g' | \
-
-# parse 3rd tab
-awk 'BEGIN{FS="\t"}; {print $3}'
-
-# sort lines alphabetically
-sort | \
+fasta_gz_input=$1
 
 # If 'ALL' is passed, give a total count of sequences
 if [ "$#" -eq 2 ]
 then
   if [ $2 == 'ALL' ]
   then
-    echo "The total number of sequences is:"
-    -l wc -l
-    echo " "
+    # Report
+    echo -e "The total number of sequences is:"
+
+    # count lines
+    zgrep "^>" "$fasta_gz_input" | \
+    wc -l
+    echo -e " "
   fi
-fi | \
+fi
 
-# tally unique lines
+# Report
+echo -e "The numbers of sequences by country are:"
+
+# Pipe just opening lines with grep. Don't store whole lines!
+zgrep "^>" "$fasta_gz_input" | \
+
+# Replace | with \t
+sed -E 's/\|/\t/g' | \
+
+# parse 3rd tab
+awk 'BEGIN{FS="\t"}; {print $3}' | \
+
+# then sort
+sort | \
+
+# sort and print uniques count (by country)
 uniq -c | \
-
-# sort numerically, reverse
 sort -nr
